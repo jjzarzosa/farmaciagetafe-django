d2f5df3c9549c0e2cd71972eb5524754ececa4e6
jjzarzosa <jjzarzosa@hotmail.com>
2021-06-28 14:24:02 +0200

    2021_06_28_OK_v2

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from carts.models import CartItem
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistrationForm, UserForm, UserProfileForm
 from .models import Account, UserProfile
-from orders.models import Order
+from orders.models import Order, OrderProduct
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -152,8 +152,10 @@ def activate(request, uidb64, token):
 def dashboard(request):
     orders = Order.objects.order_by('created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
     context = {
         'orders_count': orders_count,
+        'userprofile': userprofile,
 
     }
     return render(request, 'accounts/dashboard.html', context)
@@ -270,3 +272,18 @@ def change_password(request):
             messages.error(request, 'Las contraseñas no coinciden!')
             return redirect('change_password')
     return render(request,'accounts/change_password.html')
+
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
