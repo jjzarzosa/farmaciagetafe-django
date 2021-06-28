fbd1a7bb78b2a9ed1901f02f94a2ba6abf634c95
jjzarzosa <jjzarzosa@hotmail.com>
2021-06-29 00:12:50 +0200

    2021_06_29_Final_Ok

--- farmaciagetafe/settings.py
+++ farmaciagetafe/settings.py
@@ -44,17 +44,26 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login/'
+
 
 ROOT_URLCONF = 'farmaciagetafe.urls'
 
