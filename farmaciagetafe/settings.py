72feadb79a689924d53e540cf5f59a647b6cf451
jjzarzosa <jjzarzosa@hotmail.com>
2021-06-28 17:05:45 +0200

    2021_06_28_Final_project_OK

--- farmaciagetafe/settings.py
+++ farmaciagetafe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i+cu!%@e$1ju5$=p9$!3zpl3084w)^8t&$z+)-&g4ow8*we&3h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -146,6 +147,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'farmacia.madrid.getafe@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Product, Variation, ReviewRating
+from .models import Product, ProductGallery, Variation, ReviewRating, ProductGallery
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
+import admin_thumbnails
+
 
 # Register your models here.
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class ProductAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','fecha_modificacion', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -18,3 +25,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
